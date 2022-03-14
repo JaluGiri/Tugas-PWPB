c016138204d7d0e69953f94e0daadb38db9d2df5
JaluGiri <101552745+JaluGiri@users.noreply.github.com>
2022-03-14 16:07:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>   
+</head>
+ 
+<body>
+    <section>
+        <!--- navigation --->
+        <nav>
+            <!--- logo --->
+            <a href="#" class="logo">Hello!</a>
+            <!--- menu --->
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">School</a></li>
+                <li><a href="#">Friends</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+ 
+        <!--- text --->
+        <center>
+            <div class="text-container">
+            <p>Hello,</p>
+            <p>I&#8217;M JaluGiri</p>
+            <p>Student In SMKN 1 CIAMIS</p>
+            <p>here you will find hehe
+            <br>Let's join with us</p>
+            <button class="hire-btn"><a href="formLogin.php">LOGIN</a></button>
+            <button class="down-cv"><a href="formRegister.php">REGISTER</a></button>
+        </div>
+        <!--- text --->
+        <img alt="model" class="model" src="BMO.gif" >
+    </center>
+    </section>  
+ 
+   
+</body>
+ 
+</html>
